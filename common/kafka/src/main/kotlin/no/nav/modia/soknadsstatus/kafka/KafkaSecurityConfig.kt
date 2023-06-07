0ed11872481ef26c2ce22177c85dfc3df382524a
Jesper Paulsen <jesper.gayosso.paulsen@nav.no>
2023-06-07 11:02:41 +0200

    [KAIZEN-0] Oppdatere kafka env variabler

--- common/kafka/src/main/kotlin/no/nav/modia/soknadsstatus/kafka/KafkaSecurityConfig.kt
+++ common/kafka/src/main/kotlin/no/nav/modia/soknadsstatus/kafka/KafkaSecurityConfig.kt
@@ -5,9 +5,9 @@ import no.nav.personoversikt.common.utils.EnvUtils
 data class KafkaSecurityConfig(
     val aivenBootstrapServers: String = EnvUtils.getRequiredConfig("KAFKA_BROKERS"),
     val aivenCredstorePassword: String = EnvUtils.getRequiredConfig("KAFKA_CREDSTORE_PASSWORD"),
-    val aivenKeystoreLocation: String = EnvUtils.getRequiredConfig("KAFKA_AIVEN_KEYSTORE_LOCATION"),
-    val aivenSecurityProtocol: String = EnvUtils.getRequiredConfig("KAFKA_AIVEN_SECURITY_PROTOCOL"),
-    val aivenTruststoreLocation: String = EnvUtils.getRequiredConfig("KAFKA_AIVEN_TRUSTOSTORE_LOCATION"),
+    val aivenKeystoreLocation: String = EnvUtils.getRequiredConfig("KAFKA_KEYSTORE_PATH"),
+    val aivenSecurityProtocol: String = "SSL",
+    val aivenTruststoreLocation: String = EnvUtils.getRequiredConfig("KAFKA_TRUSTSTORE_PATH"),
     val aivenSchemaRegistryUrl: String = EnvUtils.getRequiredConfig("KAFKA_SCHEMA_REGISTRY"),
     val aivenRegistryUser: String = EnvUtils.getRequiredConfig("KAFKA_SCHEMA_REGISTRY_USER"),
     val aivenRegistryPassword: String = EnvUtils.getRequiredConfig("KAFKA_SCHEMA_REGISTRY_PASSWORD"),
