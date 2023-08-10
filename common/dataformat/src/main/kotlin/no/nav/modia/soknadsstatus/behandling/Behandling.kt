c94a2b8eca26e4e6f1ceb8765a54e91ee570182a
Jesper Paulsen <jesper.gayosso.paulsen@nav.no>
2023-08-10 10:50:38 +0200

    [KAIZEN-0] Slett ubrukt kode

--- common/dataformat/src/main/kotlin/no/nav/modia/soknadsstatus/behandling/Behandling.kt
+++ common/dataformat/src/main/kotlin/no/nav/modia/soknadsstatus/behandling/Behandling.kt
@@ -1,17 +1,7 @@
 package no.nav.modia.soknadsstatus.behandling
 
 import kotlinx.datetime.LocalDateTime
-import kotlinx.serialization.DeserializationStrategy
 import kotlinx.serialization.Serializable
-import kotlinx.serialization.json.Json
-import kotlinx.serialization.json.JsonContentPolymorphicSerializer
-import kotlinx.serialization.json.JsonElement
-import kotlinx.serialization.json.jsonObject
-import no.nav.modia.soknadsstatus.Encoding
-import org.apache.kafka.common.serialization.Deserializer
-import org.apache.kafka.common.serialization.Serde
-import org.apache.kafka.common.serialization.Serializer
-import java.nio.charset.StandardCharsets
 
 @Serializable
 sealed class Behandling {
@@ -30,40 +20,4 @@ sealed class Behandling {
     abstract val sakstema: Sakstema
     abstract val sekundaerBehandlingREF: List<SekundaerBehandlingREF>
     abstract val styringsinformasjonListe: List<StyringsinformasjonListe>
-}
-
-object BehandlingJsonSerdes {
-    class BehandlingJsonSerializer : Serializer<Behandling> {
-        override fun serialize(topic: String?, data: Behandling?): ByteArray {
-            if (data == null) {
-                return ByteArray(0)
-            }
-            val encodedBehandling = Encoding.encode(Behandling.serializer(), data)
-            return encodedBehandling.encodeToByteArray()
-        }
-    }
-
-    class BehandlingJsonDeserializer : Deserializer<Behandling> {
-        override fun deserialize(topic: String?, data: ByteArray?): Behandling? {
-            if (data == null) {
-                return null
-            }
-
-            val encodedString = String(data, StandardCharsets.UTF_8)
-            return Json.decodeFromString(BehandlingSerializer, encodedString)
-        }
-    }
-
-    class JsonSerde : Serde<Behandling> {
-        override fun serializer() = BehandlingJsonSerializer()
-
-        override fun deserializer() = BehandlingJsonDeserializer()
-    }
-}
-
-object BehandlingSerializer : JsonContentPolymorphicSerializer<Behandling>(Behandling::class) {
-    override fun selectDeserializer(element: JsonElement): DeserializationStrategy<Behandling> = when {
-        "avslutningsstatus" in element.jsonObject -> BehandlingAvsluttet.serializer()
-        else -> BehandlingOpprettet.serializer()
-    }
 }
