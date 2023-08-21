4be5cf4ca53ccfecc88687cb2268e07d2e8ec026
Jesper Paulsen <jesper.gayosso.paulsen@nav.no>
2023-08-21 10:03:41 +0200

    [KAIZEN-0] Opprette migreringsfil for å lagre historikken

---
+++ apps/modia-soknadsstatus-api/src/main/resources/db/migration/V1_3_behandling_table.sql
@@ -0,0 +1,60 @@
+DROP TABLE IF EXISTS soknadsstatus;
+
+CREATE TABLE identer
+(
+    ident VARCHAR(11) UNIQUE
+);
+
+
+CREATE TABLE behandlinger
+(
+    id                    UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    behandling_id         VARCHAR(20),
+    produsent_system      VARCHAR(20) NOT NULL,
+    start_tidspunkt       TIMESTAMPTZ NOT NULL,
+    slutt_tidspunkt       TIMESTAMPTZ,
+    sist_oppdatert        TIMESTAMPTZ NOT NULL,
+    sakstema              VARCHAR(10),
+    behandlingstema       VARCHAR(10),
+    status                statusEnum  NOT NULL,
+    ansvarlig_enhet       VARCHAR(20) NOT NULL,
+    primaer_behandling_id VARCHAR(20)
+);
+
+
+
+CREATE TABLE sekundaer_behandlinger
+(
+    fra_behandling VARCHAR(20) NOT NULL,
+    til_behandling VARCHAR(20) NOT NULL,
+    PRIMARY KEY (fra_behandling, til_behandling)
+);
+
+CREATE TABLE behandling_eiere
+(
+    ident         VARCHAR(11) REFERENCES identer (ident) ON DELETE CASCADE,
+    behandling_id VARCHAR(20) REFERENCES behandlinger (behandling_id) ON DELETE CASCADE,
+    PRIMARY KEY (ident, behandling_id)
+);
+
+CREATE TABLE hendelser
+(
+    id                 UUID PRIMARY KEY DEFAULT gen_random_uuid(),
+    hendelses_id       VARCHAR(20),
+    behandling_id      UUID REFERENCES behandlinger (id) ON DELETE CASCADE,
+    hendelse_produsent VARCHAR(20) NOT NULL,
+    hendelse_tidspunkt TIMESTAMPTZ NOT NULL,
+    hendelse_type      VARCHAR(20) NOT NULL,
+    status             statusEnum  NOT NULL,
+    ansvarlig_enhet    VARCHAR(20) NOT NULL,
+    produsent_system   VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE hendelse_eiere
+(
+    ident       VARCHAR(11) REFERENCES identer (ident) ON DELETE CASCADE,
+    hendelse_id VARCHAR(20) REFERENCES hendelser (hendelses_id) ON DELETE CASCADE,
+    PRIMARY KEY (ident, hendelse_id)
+);
+
+
