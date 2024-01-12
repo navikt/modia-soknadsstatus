e7d1a5c7383af86f84e12284fc223172d39fe77e
Jesper Paulsen <jesper.gayosso.paulsen@nav.no>
2024-01-12 14:26:06 +0100

    [KAIZEN-0] Rydde bort kode for å fikse et DL

--- apps/arena-infotrygd-soknadsstatus-transform/src/main/kotlin/no/nav/modia/soknadsstatus/ArenaInfotrygdAvslutningsstatusMapper.kt
+++ apps/arena-infotrygd-soknadsstatus-transform/src/main/kotlin/no/nav/modia/soknadsstatus/ArenaInfotrygdAvslutningsstatusMapper.kt
@@ -11,10 +11,7 @@ object ArenaInfotrygdAvslutningsstatusMapper : AvslutningsStatusMapper {
     override fun getAvslutningsstatus(
         produsentSystem: String,
         status: String,
-    ): SoknadsstatusDomain.Status {
-        if (status == "BT") return SoknadsstatusDomain.Status.AVBRUTT
-        return mapper.getMappedStatus(produsentSystem, status)
-    }
+    ): SoknadsstatusDomain.Status = mapper.getMappedStatus(produsentSystem, status)
 }
 
 private class AvslutningsMapper : CSVLoader(FILE_NAME) {
