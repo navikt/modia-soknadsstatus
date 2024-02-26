3a95a482eb4fe9743b5fa1dc78cbb0f08bcbcca5
abrha <abrha.kiros.abrha@nav.no>
2024-02-26 10:20:53 +0100

    [KAIZEN-0] fjern duplikat hendelser

---
+++ apps/modia-soknadsstatus-api/src/main/resources/db/migration/V1_5__fjern_duplikat_hendelser.sql
@@ -0,0 +1,12 @@
+-- step 1
+CREATE TABLE hendelser_temp (LIKE hendelser);
+
+-- step 2
+INSERT INTO hendelser_temp(id, hendelses_id, modia_behandling_id, hendelse_produsent, hendelse_tidspunkt, hendelse_type, status, ansvarlig_enhet, behandling_id, behandlingstema, behandlingstype)
+SELECT DISTINCT ON (modia_behandling_id, hendelses_id, status) id, hendelses_id, modia_behandling_id, hendelse_produsent, hendelse_tidspunkt, hendelse_type, status, ansvarlig_enhet, behandling_id, behandlingstema, behandlingstype
+FROM hendelser;
+
+-- step 3
+ALTER TABLE hendelser RENAME TO hendelser_old;
+ALTER TABLE hendelser_temp RENAME TO hendelser;
+
