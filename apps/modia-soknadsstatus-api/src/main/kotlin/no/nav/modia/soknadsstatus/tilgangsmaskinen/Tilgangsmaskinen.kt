52f74bad498e1d7c76f757f872b9f1fd03cb953f
abrha <abrha.kiros.abrha@nav.no>
2025-11-05 13:04:40 +0100

    [KAIZEN-0] Tilgangsmaskinen tester.

--- apps/modia-soknadsstatus-api/src/main/kotlin/no/nav/modia/soknadsstatus/tilgangsmaskinen/Tilgangsmaskinen.kt
+++ apps/modia-soknadsstatus-api/src/main/kotlin/no/nav/modia/soknadsstatus/tilgangsmaskinen/Tilgangsmaskinen.kt
@@ -7,12 +7,12 @@ import no.nav.common.health.selftest.SelfTestCheck
 import no.nav.common.types.identer.Fnr
 import no.nav.common.utils.UrlUtils
 import no.nav.modia.soknadsstatus.consumer.tilgangsmaskinen.generated.apis.TilgangControllerApi
-import no.nav.modia.soknadsstatus.consumer.tilgangsmaskinen.generated.infrastructure.*
+import no.nav.modia.soknadsstatus.consumer.tilgangsmaskinen.generated.infrastructure.ClientException
+import no.nav.modia.soknadsstatus.consumer.tilgangsmaskinen.generated.infrastructure.ServerException
 import no.nav.modia.soknadsstatus.infratructure.ping.Pingable
 import no.nav.modia.soknadsstatus.utils.CacheUtils
 import no.nav.personoversikt.common.logging.TjenestekallLogg
 import okhttp3.OkHttpClient
-import org.slf4j.LoggerFactory
 
 interface Tilgangsmaskinen : Pingable {
     fun sjekkTilgang(fnr: Fnr): TilgangsMaskinResponse?
@@ -27,17 +27,20 @@ open class TilgangsmaskinenImpl(
     private val url: String,
     private val client: OkHttpClient,
     private val cache: Cache<String, TilgangsMaskinResponse?> = CacheUtils.createCache(),
+    private val tilgangsMaskinenApi: TilgangControllerApi = TilgangControllerApi(url, client),
 ) : Tilgangsmaskinen {
-    private val tilgangsMaskinenApi = TilgangControllerApi(url, client)
-    private val logger = LoggerFactory.getLogger(TilgangsmaskinenImpl::class.java)
-
     override fun sjekkTilgang(fnr: Fnr): TilgangsMaskinResponse? =
         cache.get(fnr.get()) {
             try {
                 tilgangsMaskinenApi.kompletteRegler(fnr.get())
                 TilgangsMaskinResponse(harTilgang = true)
             } catch (e: ClientException) {
-                makeErrorResponse(e)
+                TjenestekallLogg.error("ClientException", throwable = e, fields = mapOf("fnr" to fnr.get()))
+                if (e.statusCode == 403) {
+                    TilgangsMaskinResponse(harTilgang = false, error = null)
+                } else {
+                    null
+                }
             } catch (e: ServerException) {
                 TjenestekallLogg.error("ServerException", throwable = e, fields = mapOf("fnr" to fnr.get()))
                 TilgangsMaskinResponse(harTilgang = false)
@@ -50,20 +53,6 @@ open class TilgangsmaskinenImpl(
                 null
             }
         }
-
-    private fun makeErrorResponse(response: ClientException): TilgangsMaskinResponse {
-        if (response.statusCode == 403) {
-            response as ClientError<*>
-            try {
-                return TilgangsMaskinResponse(harTilgang = false)
-            } catch (e: Exception) {
-                logger.warn("Parse exception when parsing error response from tilgangsmaskinen: ${e.message}")
-                return TilgangsMaskinResponse(harTilgang = false)
-            }
-        }
-        logger.warn("Received error response from tilgangsmaskinen: ${response.statusCode}")
-        return TilgangsMaskinResponse(harTilgang = false)
-    }
 
     override fun ping() =
         SelfTestCheck(
