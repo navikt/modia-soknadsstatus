b3a9ed267bff813cb04d4d5093ce50f4d9d16031
Abrha.Kiros.Abrha <abrha.kiros.abrha@nav.no>
2026-01-30 14:33:17 +0100

    [KAIZEN-0] logging fix

--- apps/mq-to-kafka/src/main/kotlin/no/nav/modia/soknadsstatus/MqToKafka.kt
+++ apps/mq-to-kafka/src/main/kotlin/no/nav/modia/soknadsstatus/MqToKafka.kt
@@ -74,7 +74,6 @@ fun Application.mqToKafkaModule() {
                         }
                     }
             } catch (e: Exception) {
-                //slackClient?.postMessage("MqToKafkaModule: Klarte ikke å koble til MQ: ${mqConfig.mqQueue}")
                 TjenestekallLogg.error(
                     header = "MqToKafkaModule: Klarte ikke å koble til MQ:",
                     fields = mapOf(),
